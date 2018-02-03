06ec41cc0e53e5737c51e96042201fe6ee4f98ed
Yariv Zur <yarivtzur@gmail.com>
2018-02-03 18:25:26 +0200

    Refactor the protein preprocessing to be done in parallel in separate module
    Move tha arg marsing to util method

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import argparse
-import gzip
 import multiprocessing
 import sys
 import logging
 import os
-
-from multiprocessing import Queue
-from Bio import SeqIO
+from subprocess import call
 from constants import STRAINS_DIR, DATA_DIR
 from ftp_handler import download_strain_files
 from logging_config import listener_process, listener_configurer, worker_configurer, log_queue
@@ -14,14 +11,7 @@ from protein_preprocessor import create_all_strains_file_with_indices
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Data processing pipeline for pseudogene search '
-                                                 'in Pseudomonas Areguinosa strains')
-    parser.add_argument('-d', '--download', action="store_true",
-                        help='Download all valid PA strains from the refseq ftp for analysis')
-    parser.add_argument('-s', '--sample', type=int, dest='sample_size', default=None,
-                        help='Specify a sample size to limit the amount of strains downloaded')
-    parser.add_argument('-c', '--cluster', action="store_true",
-                        help='Run CD-HIT clustering on downloaded PA strains proteins')
+    parser = init_args_parser()
     args = parser.parse_args()
     if not len(sys.argv) > 1:
         parser.print_help()
@@ -49,69 +39,26 @@ def main():
         listener.join()
 
 
-def create_all_strains_file_with_indices():
-    """
-    Add indices to each strain sequence header for easier parsing
-    and combine all strains protein sequences into single fasta file for clustering
-    """
-    logger = logging.getLogger()
-    logger.info("Indexing proteins by their strain index & protein index in strain gene")
-    aggregated_proteins_file_path = os.path.join(DATA_DIR, 'all_strains_proteins.fasta')
-    if os.path.exists(aggregated_proteins_file_path):
-        os.remove(aggregated_proteins_file_path)
-    for root, strain_dirs, files in os.walk(STRAINS_DIR):
-        for strain_dir in strain_dirs:
-            strain_dir_files = os.listdir(os.path.join(root, strain_dir))
-            protein_file_name = [f for f in strain_dir_files if 'protein.faa' in f][0]
-            cds_file_name = [f for f in strain_dir_files if 'cds_from_genomic.fna' in f][0]
-            if not protein_file_name or not cds_file_name:
-                logger.warning("Could not find protein file or cds_from_genomic file for strain %s, skipping" % strain_dir)
-                continue
-            protein_file = cds_file = aggregated_proteins_file = strain_index_file = None
-            try:
-                strain_index_file = open(os.path.join(root, strain_dir, 'strain_index'))
-                strain_index = '[' + strain_index_file.readline() + ']'
-                if protein_file_name.endswith('gz'):
-                    protein_file = gzip.open(os.path.join(root, strain_dir, protein_file_name), 'rt')
-                else:
-                    protein_file = open(os.path.join(root, strain_dir, protein_file_name))
-                if cds_file_name.endswith('gz'):
-                    cds_file = gzip.open(os.path.join(root, strain_dir, cds_file_name), 'rt')
-                else:
-                    cds_file = open(os.path.join(root, strain_dir, cds_file_name))
-
-                strain_cds_seq_dict = SeqIO.to_dict(SeqIO.parse(cds_file, 'fasta'))
-                strain_cds_seq_headers = list(strain_cds_seq_dict.keys())
-                strain_protein_seq_iter = SeqIO.parse(protein_file, 'fasta')
-                for strain_protein_seq in strain_protein_seq_iter:
-                    protein_id = strain_protein_seq.id
-                    cds_protein_header = [h for h in strain_cds_seq_headers if protein_id in h][0]
-                    protein_index_in_gene = '[' + cds_protein_header[cds_protein_header.rfind('_') + 1:] + ']'
-                    strain_protein_seq.description = strain_index + protein_index_in_gene + strain_protein_seq.description
-                    strain_protein_seq.id = ""
-                    aggregated_proteins_file = open(aggregated_proteins_file_path, 'a+')
-                    SeqIO.write(strain_protein_seq, aggregated_proteins_file, 'fasta')
-                logger.info("Strain %s proteins were indexed and written to file" % strain_dir[strain_dir.rfind(']') + 1:])
-            finally:
-                if protein_file is not None:
-                    protein_file.close()
-                if cds_file is not None:
-                    cds_file.close()
-                if aggregated_proteins_file is not None:
-                    aggregated_proteins_file.close()
-                if strain_index_file is not None:
-                    strain_index_file.close()
-    logger.info("Finished adding all strains protein sequences to combined file")
-    return aggregated_proteins_file_path
+def init_args_parser():
+    parser = argparse.ArgumentParser(description='Data processing pipeline for pseudogene search '
+                                                 'in Pseudomonas Areguinosa strains')
+    parser.add_argument('-d', '--download', action="store_true",
+                        help='Download all valid PA strains from the refseq ftp for analysis')
+    parser.add_argument('-s', '--sample', type=int, dest='sample_size', default=None,
+                        help='Specify a sample size to limit the amount of strains downloaded')
+    parser.add_argument('-c', '--cluster', action="store_true",
+                        help='Run CD-HIT clustering on downloaded PA strains proteins')
+    return parser
 
 
 def perform_clustering_on_strains(aggregated_proteins_file_path):
     """Run the CD-HIT program to perform clustering on the strains"""
     logger = logging.getLogger()
     logger.info("Running CD-HIT on combined proteins file to create clustering")
-    clusterring_output_file = os.path.join(DATA_DIR, 'protein_clusters.txt')
-    cd_hit_args = ['cd-hit', '-i', aggregated_proteins_file_path, '-o', clusterring_output_file, '-c 0.70',
+    clustering_output_file = os.path.join(DATA_DIR, 'protein_clusters.txt')
+    cd_hit_args = ['cd-hit', '-i', aggregated_proteins_file_path, '-o', clustering_output_file, '-c 0.70',
                    '-n 5', '-M 16000', '-g 1', '-p 1']
+    call(cd_hit_args)
 
 
 if __name__ == '__main__':
---
+++ protein_preprocessor.py
@@ -0,0 +1,104 @@
+import gzip
+import logging
+import multiprocessing
+import os
+import shutil
+
+from Bio import SeqIO
+
+from logging_config import worker_configurer, log_queue
+from constants import DATA_DIR, STRAINS_DIR, NUMBER_OF_PROCESSES, FASTA_FILE_TYPE, PROTEIN_FILE_PATTERN, \
+    CDS_FROM_GENOMIC_PATTERN, STRAIN_INDEX_FILE
+
+COMBINED_STRAIN_PROTEINS_PREFIX = "combined_strain_proteins"
+WORKER_PROTEIN_FILE_PREFIX = COMBINED_STRAIN_PROTEINS_PREFIX + "_worker_"
+
+
+def create_all_strains_file_with_indices():
+    """
+    Preprocess all strains proteins in parallel and combine all worker output files into single fasta file for clustering
+    """
+    logger = logging.getLogger()
+    logger.info("Indexing proteins by their strain index & protein index in strain gene")
+    all_proteins_file_path = os.path.join(DATA_DIR, COMBINED_STRAIN_PROTEINS_PREFIX + "_all.fasta")
+    for file in os.listdir(DATA_DIR):
+        if COMBINED_STRAIN_PROTEINS_PREFIX in file:
+            os.remove(os.path.join(DATA_DIR, file))
+    job_queue = multiprocessing.Queue()
+    prepare_preprocessing_jobs(job_queue)
+    workers = [multiprocessing.Process(target=preprocess_strain_proteins, args=(i, job_queue, worker_configurer))
+               for i in range(NUMBER_OF_PROCESSES)]
+    for w in workers:
+        w.start()
+    job_queue.put(None)
+    for w in workers:
+        w.join()
+    worker_combined_protein_files = [f for f in os.listdir(DATA_DIR) if WORKER_PROTEIN_FILE_PREFIX in f]
+    with open(all_proteins_file_path, 'w') as dstd:
+        for worker_file in worker_combined_protein_files:
+            with open(os.path.join(DATA_DIR, worker_file), 'r') as srcd:
+                shutil.copyfileobj(srcd, dstd)
+
+
+def prepare_preprocessing_jobs(job_queue):
+    """Put all downloaded strain dirs in job queue for workers"""
+    downloaded_strains = os.listdir(STRAINS_DIR)
+    for strain_dir in downloaded_strains:
+        job_queue.put(strain_dir)
+
+
+def preprocess_strain_proteins(worker_id, job_queue, configurer):
+    """
+    Preprocess all proteins for downloaded strains by indexing according to strain index and protein index within
+    strain genome, using the cds_from_genome file
+    """
+    configurer(log_queue)
+    logger = logging.getLogger(__name__ + "_worker_" + str(worker_id))
+    worker_combined_proteins_file_path = os.path.join(DATA_DIR, WORKER_PROTEIN_FILE_PREFIX + str(worker_id))
+    while True:
+        strain_dir = job_queue.get()
+        if strain_dir is None:
+            job_queue.put(None)
+            break
+        strain_dir_files = os.listdir(os.path.join(STRAINS_DIR, strain_dir))
+        protein_file_name = [f for f in strain_dir_files if PROTEIN_FILE_PATTERN in f][0]
+        cds_file_name = [f for f in strain_dir_files if CDS_FROM_GENOMIC_PATTERN in f][0]
+        if not protein_file_name or not cds_file_name:
+            logger.warning(
+                "Could not find protein file or cds_from_genomic file for strain %s, skipping" % strain_dir)
+            continue
+        protein_file = cds_file = strain_index_file = worker_combined_proteins_file = None
+        try:
+            strain_index_file = open(os.path.join(STRAINS_DIR, strain_dir, STRAIN_INDEX_FILE))
+            strain_index = '[' + strain_index_file.readline() + ']'
+            if protein_file_name.endswith('gz'):
+                protein_file = gzip.open(os.path.join(STRAINS_DIR, strain_dir, protein_file_name), 'rt')
+            else:
+                protein_file = open(os.path.join(STRAINS_DIR, strain_dir, protein_file_name))
+            if cds_file_name.endswith('gz'):
+                cds_file = gzip.open(os.path.join(STRAINS_DIR, strain_dir, cds_file_name), 'rt')
+            else:
+                cds_file = open(os.path.join(STRAINS_DIR, strain_dir, cds_file_name))
+
+            strain_cds_seq_dict = SeqIO.to_dict(SeqIO.parse(cds_file, FASTA_FILE_TYPE))
+            strain_cds_seq_headers = list(strain_cds_seq_dict.keys())
+            strain_protein_seq_iter = SeqIO.parse(protein_file, FASTA_FILE_TYPE)
+            for strain_protein_seq in strain_protein_seq_iter:
+                protein_id = strain_protein_seq.id
+                cds_protein_header = [h for h in strain_cds_seq_headers if protein_id in h][0]
+                protein_index_in_gene = '[' + cds_protein_header[cds_protein_header.rfind('_') + 1:] + ']'
+                strain_protein_seq.description = strain_index + protein_index_in_gene + strain_protein_seq.description
+                strain_protein_seq.id = ""
+                worker_combined_proteins_file = open(worker_combined_proteins_file_path, 'a+')
+                SeqIO.write(strain_protein_seq, worker_combined_proteins_file, FASTA_FILE_TYPE)
+            logger.info(
+                "Strain %s proteins were indexed and written to file" % strain_dir[strain_dir.rfind(']') + 1:])
+        finally:
+            if protein_file is not None:
+                protein_file.close()
+            if cds_file is not None:
+                cds_file.close()
+            if worker_combined_proteins_file is not None:
+                worker_combined_proteins_file.close()
+            if strain_index_file is not None:
+                strain_index_file.close()
