d4f1fb12b5e31de3ddb660b73382ea09f390d89c
Yariv Zur <yarivtzur@gmail.com>
2018-03-17 01:39:49 +0200

    debugging issue with getting reps
    Signed-off-by: Yariv Zur <yarivtzur@gmail.com>

--- nucleotide_preprocessor.py
+++ nucleotide_preprocessor.py
@@ -56,13 +56,16 @@ def create_representatives_and_pseudogenes_file(log_queue):
 def get_clusters_representatives(clusters_file):
     representatives = {}
     with open(clusters_file, 'r') as clusters_db:
+        print("getting reps")
         for line in clusters_db:
             if line.startswith(">Cluster"):
                 cluster_index = int(line.split()[-1])
+                print("got cluster " + str(cluster_index))
             else:
                 if line.endswith("*"):
                     match = CLUSTER_STRAIN_PATTERN.match(line)
                     strain_index = int(match.group(1))
+                    print("adding rep " + match.group(0))
                     strain_reps = representatives[strain_index] if strain_index in representatives.keys() else []
                     strain_reps.append(Representative(int(match.group(2)), cluster_index))
                     representatives[strain_index] = strain_reps
