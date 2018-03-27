955ce2ef70ad7ef76dafb0ff70cddfd6b0dbabc0
Yariv Zur <yarivtzur@gmail.com>
2018-03-28 01:36:32 +0300

    fix charting bar chart with dataframe indices
    Signed-off-by: Yariv Zur <yarivtzur@gmail.com>

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def main():
             logger.info("Plotting strains to clusters histogram")
             set_labels_font_size()
             stats_by_total_clusters = stats_df.sort_values('total_clusters', ascending=False)
-            plt.bar(stats_by_total_clusters['total_clusters'])
+            plt.bar(stats_by_total_clusters.index.values, stats_by_total_clusters['total_clusters'])
             plt.xlabel("strains #")
             plt.ylabel("clusters #")
             plt.title("strains to clusters bar chart")
