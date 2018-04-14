59956b7a70edfee59f1f4a0276a05b89be2e70f0
Yariv Zur <yarivtzur@gmail.com>
2018-04-15 01:31:03 +0300

    add debug print
    Signed-off-by: Yariv Zur <yarivtzur@gmail.com>

--- data_analysis.py
+++ data_analysis.py
@@ -261,7 +261,8 @@ def get_2nd_stage_stats_per_strain(first_stage_data):
     for cluster in second_stage_clusters_map.values():
         clusters_df.loc[cluster.index]['total_strains'] = cluster.get_cluster_strains_num()
         clusters_df.loc[cluster.index]['1st_stage_reps'] = cluster.member_1st_stage_reps
-        if len(cluster.member_1st_stage_reps) > 1 or len(cluster.member_1st_stage_reps[0]) > 1:
+        print(cluster.member_1st_stage_reps)
+        if len(cluster.member_1st_stage_reps) > 1 or len(cluster.member_1st_stage_reps.values()) > 1:
             print("cluster %s has more than one rep" % cluster.index)
         elif len(cluster.member_1st_stage_reps) == 1:
             [(representative_strain_index, representative_seq_index)] = cluster.member_1st_stage_reps.items()
