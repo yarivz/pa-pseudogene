d951eaef002f11d13ed7f56fba0f5dbb1784f2d1
Yariv Zur <yarivtzur@gmail.com>
2018-04-15 03:13:52 +0300

    fix print format string syntax
    Signed-off-by: Yariv Zur <yarivtzur@gmail.com>

--- data_analysis.py
+++ data_analysis.py
@@ -250,7 +250,7 @@ def get_2nd_stage_stats_per_strain(first_stage_data):
         clusters_df.loc[cluster.index]['total_strains'] = cluster.get_cluster_strains_num()
         clusters_df.loc[cluster.index]['1st_stage_reps'] = cluster.member_1st_stage_reps
         if len(cluster.member_1st_stage_reps) > 1 or len(cluster.member_1st_stage_reps.values()) > 1:
-            print("cluster %s has more than one rep: %s" % cluster.index, cluster.member_1st_stage_reps)
+            print("cluster %s has more than one rep: %s" % (cluster.index, cluster.member_1st_stage_reps))
         elif len(cluster.member_1st_stage_reps) == 1:
             [(representative_strain_index, representative_seq_index)] = cluster.member_1st_stage_reps.items()
             representative_cluster_id = first_stage_strain_seq_cluster_map[representative_strain_index].seq_clusters[representative_seq_index[0]]
